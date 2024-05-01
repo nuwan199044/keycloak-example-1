5a4f54f29ccf4ea336a5a9bb9a943a4cc151c6b6
Nuwan <nuwan199044@gmail.com>
2024-05-01 23:21:46 +0530

    Implementing the method level security with roles.

--- src/main/java/com/mycompany/restaurantapp/config/SecurityConfig.java
+++ src/main/java/com/mycompany/restaurantapp/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.mycompany.restaurantapp.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,7 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(t -> t.disable());
@@ -22,7 +28,8 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
         );
         http.oauth2ResourceServer(t ->
-                t.opaqueToken(Customizer.withDefaults())
+                t.jwt(configurer -> configurer.jwtAuthenticationConverter(jwtAuthConverter))
+//                t.opaqueToken(Customizer.withDefaults())
         );
         http.sessionManagement(
                 t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/java/com/mycompany/restaurantapp/service/MenuItemService.java
+++ src/main/java/com/mycompany/restaurantapp/service/MenuItemService.java
@@ -1,13 +1,25 @@
 package com.mycompany.restaurantapp.service;
 
+import com.mycompany.restaurantapp.dto.MenuItemDTO;
+import com.mycompany.restaurantapp.entity.MenuItem;
 import com.mycompany.restaurantapp.repository.MenuItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MenuItemService {
     private final MenuItemRepository menuItemRepository;
+    private final ModelMapper modelMapper;
 
-
+    public MenuItemDTO updateMenuItemPrice(Long menuItemId, Double newPrice) {
+        Optional<MenuItem> optionalMenuItem = menuItemRepository.findById(menuItemId);
+        return optionalMenuItem.map(menuItem -> {
+            menuItem.setPrice(newPrice);
+            return modelMapper.map(menuItemRepository.save(menuItem), MenuItemDTO.class);
+        }).orElse(null);
+    }
 }
