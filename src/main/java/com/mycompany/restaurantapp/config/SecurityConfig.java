412c494aeae8662a1ee9f76b0201e5afb4fd661d
Nuwan <nuwan199044@gmail.com>
2024-05-04 12:11:29 +0530

    Whitelisting the public URLs and adding JwtAuthConverter to security config.

--- src/main/java/com/mycompany/restaurantapp/config/SecurityConfig.java
+++ src/main/java/com/mycompany/restaurantapp/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.mycompany.restaurantapp.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,15 +13,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(t -> t.disable());
         http.authorizeHttpRequests(requests ->
-                requests.anyRequest().authenticated()
+                requests.requestMatchers(HttpMethod.GET, "/restaurants/public/list").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/restaurants/public/menu/*").permitAll()
+                        .anyRequest().authenticated()
         );
         http.oauth2ResourceServer(t ->
-                t.jwt(Customizer.withDefaults())
+                t.jwt(configurer -> configurer.jwtAuthenticationConverter(jwtAuthConverter))
         );
         http.sessionManagement(
                 t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
