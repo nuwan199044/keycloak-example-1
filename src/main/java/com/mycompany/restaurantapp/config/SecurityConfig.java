062d8be05e9be2d67cd60eae804752ee0a286b77
Nuwan <nuwan199044@gmail.com>
2024-05-04 09:07:58 +0530

    Adding SecurityConfig class for enable the security.

---
+++ src/main/java/com/mycompany/restaurantapp/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.mycompany.restaurantapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf(t -> t.disable());
+        http.authorizeHttpRequests(requests ->
+                requests.anyRequest().authenticated()
+        );
+        http.oauth2ResourceServer(t ->
+                t.jwt(Customizer.withDefaults())
+        );
+        http.sessionManagement(
+                t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        return http.build();
+    }
+}
