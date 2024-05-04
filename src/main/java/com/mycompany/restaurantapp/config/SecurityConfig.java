6172acdeb4f4394494ced135859134845854e374
Nuwan <nuwan199044@gmail.com>
2024-05-04 18:33:08 +0530

    Adding filter for providing the json policy document.

--- src/main/java/com/mycompany/restaurantapp/config/SecurityConfig.java
+++ src/main/java/com/mycompany/restaurantapp/config/SecurityConfig.java
@@ -1,39 +1,48 @@
 package com.mycompany.restaurantapp.config;
 
+import org.keycloak.adapters.authorization.integration.jakarta.ServletPolicyEnforcerFilter;
+import org.keycloak.adapters.authorization.spi.ConfigurationResolver;
+import org.keycloak.adapters.authorization.spi.HttpRequest;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
+import org.keycloak.util.JsonSerialization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(t -> t.disable());
-        http.authorizeHttpRequests(requests ->
-                requests.requestMatchers(HttpMethod.GET, "/restaurants/public/list").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/restaurants/public/menu/*").permitAll()
-                        .requestMatchers("/swagger-ui/**","/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated()
-        );
-        http.oauth2ResourceServer(t ->
-                t.jwt(Customizer.withDefaults())
-        );
+        http.addFilterAfter(createPolicyEnforcerFilter(), BearerTokenAuthenticationFilter.class);
         http.sessionManagement(
                 t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
         return http.build();
+    }
+
+    private ServletPolicyEnforcerFilter createPolicyEnforcerFilter() {
+        return new ServletPolicyEnforcerFilter(new ConfigurationResolver() {
+            @Override
+            public PolicyEnforcerConfig resolve(HttpRequest httpRequest) {
+                try {
+                    return JsonSerialization.readValue(getClass().getResourceAsStream("/policy-enforcer.json"), PolicyEnforcerConfig.class);
+                } catch(IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 
     @Bean
